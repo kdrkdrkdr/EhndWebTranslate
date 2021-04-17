310dcd14e56cf5e56d106c833b0a74d8cbed09e6
kdrkdrkdr <kdrhacker1234@gmail.com>
2021-04-18 00:20:36 +0900

    update 210418

--- ehnd_web_translate.py
+++ ehnd_web_translate.py
@@ -28,15 +28,11 @@ def async_loop(func, *args):
 
 
 
-
-
 class LoadDriverWindow(QThread):
     def __init__(self, window):
         QThread.__init__(self)
         self.window = window
         self.winList = []
-
-
 
     def run(self):
         self.window.window_list.clear()
@@ -51,6 +47,25 @@ class LoadDriverWindow(QThread):
 
 
 
+class AutoTrans(QThread):
+    def __init__(self, window):
+        QThread.__init__(self)
+        self.window = window
+
+
+    def stop(self):
+        self.terminate()
+
+
+    def run(self):
+        current_url = self.window.driver.current_url
+        while True:
+            sleep(1)
+            new_url = self.window.driver.current_url
+            if current_url != new_url:
+                # self.window.show_status.setText("자동 번역 중")
+                self.window.st.start()
+                current_url = new_url
 
 
 
@@ -62,12 +77,10 @@ class TransThread(QThread):
         self.isTrans = isTrans
         self.setTerminationEnabled = True
 
-        self.ehnd_webtr_log = ""
-
 
     def stop(self):
         self.window.btnSetting(setNum=1)
-        self.window.show_status.setText("번역 중지!")
+        self.window.show_status.setText("번역 중지")
         self.terminate()
 
 
@@ -80,18 +93,14 @@ class TransThread(QThread):
             if self.isTrans:
                 self.window.show_status.setText("번역 중")
                 a = self.window.driver.find_elements_by_xpath('.//*[normalize-space(text())]')
-                
                 async_loop(self.rt, a)
-
                 self.window.sec.setText(f"{int(time()-start_time)}초")
                 self.window.show_status.setText("번역 성공")
-
                 
             else:
                 self.window.driver.refresh()
                 self.window.sec.setText("")
                 self.window.show_status.setText("원본 보기")
-
 
         except:
             self.window.show_status.setText("번역 실패")
@@ -121,8 +130,14 @@ class TransThread(QThread):
                             isJpn = findJpn.search(ih)
 
                             if isJpn != None:
-                                modified_html.append(t_j2k(japanese=ih))
+                                k = t_j2k(japanese=ih)
+                                if self.window.isPrintBoth.isChecked(): 
+                                    # k = f"{ih}({k})"
+                                    k = f"{ih}<br>{k}"
+                                modified_html.append(k)
                                 print("번역O -> ", ih)
+
+
                             else:
                                 modified_html.append(ih)
                                 print("번역X -> ", ih)
@@ -147,7 +162,8 @@ class EhndTrans(QMainWindow, Ui_MainWindow):
 
     def __init__(self):
         super().__init__()
-        self.setThread = None
+
+        self.lastUrl = ""
 
         options = webdriver.ChromeOptions()
         options.add_argument("disable-gpu") 
@@ -175,8 +191,15 @@ class EhndTrans(QMainWindow, Ui_MainWindow):
         QObject.connect(self.show_ori_btn, SIGNAL('clicked()'), self.showOri)
         QObject.connect(self.go_dev_page, SIGNAL('clicked()'), self.goDevPage)
         QObject.connect(self.reloadBtn, SIGNAL('clicked()'), self.reloadWindow)
-        QObject.connect(self.stop_trans_btn, SIGNAL('clicked()'), self.stopThread)
+        QObject.connect(self.stop_trans_btn, SIGNAL('clicked()'), self.stopTrans)
         self.window_list.currentIndexChanged.connect(self.setWindow)
+        self.isAutoTrans.stateChanged.connect(self.autoTrans)
+
+        self.st = TransThread(self, isTrans=True)
+        self.so = TransThread(self, isTrans=False)
+        self.ldw = LoadDriverWindow(self)
+        self.at = AutoTrans(self)
+
 
         self.btnSetting(setNum=1)
         self.reloadWindow()
@@ -186,15 +209,14 @@ class EhndTrans(QMainWindow, Ui_MainWindow):
 
 
     def showTrans(self):
-        st = TransThread(self, isTrans=True)
-        self.setThread = st
-        st.start()
+        self.st.start()
+
+    def stopTrans(self):
+        self.st.stop()
 
 
     def showOri(self):
-        so = TransThread(self, isTrans=False)
-        self.setThread = so
-        so.start()
+        self.so.start()
     
 
     def goDevPage(self):
@@ -202,18 +224,21 @@ class EhndTrans(QMainWindow, Ui_MainWindow):
 
 
     def reloadWindow(self):
-        ldw = LoadDriverWindow(self)
-        self.setThread = ldw
-        ldw.start()
+        self.ldw.start()
 
 
     def setWindow(self):
         dw_handles = self.driver.window_handles
         self.driver.switch_to.window(dw_handles[self.window_list.currentIndex()])
 
+
+    def autoTrans(self):
+        if self.isAutoTrans.isChecked():
+            self.at.start()
+        else:
+            self.at.stop()
     
-    def stopThread(self):
-        self.setThread.stop()
+    
 
     
     def btnSetting(self, setNum):
@@ -223,6 +248,8 @@ class EhndTrans(QMainWindow, Ui_MainWindow):
             self.window_list.setDisabled(True)
             self.reloadBtn.setDisabled(True)
             self.go_dev_page.setDisabled(True)
+            self.isPrintBoth.setDisabled(True)
+            self.isAutoTrans.setDisabled(True)
             self.stop_trans_btn.setDisabled(False)
 
         if setNum == 1:
@@ -231,6 +258,8 @@ class EhndTrans(QMainWindow, Ui_MainWindow):
             self.window_list.setDisabled(False)
             self.reloadBtn.setDisabled(False)
             self.go_dev_page.setDisabled(False)
+            self.isPrintBoth.setDisabled(False)
+            self.isAutoTrans.setDisabled(False)
             self.stop_trans_btn.setDisabled(True)
 
 
